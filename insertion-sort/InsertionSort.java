f3bdf5422df32a1707dd2b548826e67d56d54018
Suhani Patel <patelsuhani07@gmail.com>
2024-08-24 02:23:02 -0400

    feat: implementation of insertion sort

---
+++ insertion-sort/InsertionSort.java
@@ -0,0 +1,26 @@
+public class InsertionSort {
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 11, 13, 5, 6 };
+        sort(arr);
+        printArray(arr);
+    }
+}
